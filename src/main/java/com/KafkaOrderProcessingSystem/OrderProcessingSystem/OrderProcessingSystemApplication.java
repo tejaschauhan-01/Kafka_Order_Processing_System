7814daacc8419ec4e4e345eb8d661021d836d20f
pranaygupta-dev <pranay.gupta@tntar.io>
2025-11-03 15:35:09 +0530

    Initial Commit

---
+++ src/main/java/com/KafkaOrderProcessingSystem/OrderProcessingSystem/OrderProcessingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.KafkaOrderProcessingSystem.OrderProcessingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderProcessingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderProcessingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/KafkaOrderProcessingSystem/OrderProcessingSystem/OrderProcessingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KafkaOrderProcessingSystem.OrderProcessingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderProcessingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

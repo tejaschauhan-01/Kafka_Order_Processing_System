379f10e743b33d58c5dd0561629ea6d73413a352
pranaygupta-dev <pranay.gupta@tntar.io>
2025-11-10 11:20:23 +0530

    Commenting Done in Service Layers

--- src/main/java/com/KafkaOrderProcessingSystem/OrderProcessingSystem/utils/WarehouseStockUpdate.java
+++ src/main/java/com/KafkaOrderProcessingSystem/OrderProcessingSystem/utils/WarehouseStockUpdate.java
@@ -16,14 +16,23 @@ public class WarehouseStockUpdate {
     @Autowired
     private WarehouseRepository warehouseRepository;
 
+    // Method to process the received order and update warehouse stock accordingly
     public void processOrder(Order order) {
 
+        // Log that the consumer has received an order from Kafka
         log.info("kafka consumer received order: " + order);
+
+        // Fetch product stock from a database based on product name
         Optional<WarehouseStock> stockOptional = warehouseRepository.findById(order.getProductName());
 
+        // Retrieve the actual stock record (assuming product exists)
         WarehouseStock stock = stockOptional.get();
+
+        // Calculate the remaining stock after fulfilling the order
         int remaining = stock.getAvailableQuantity() - order.getQuantity();
         stock.setAvailableQuantity(remaining);
+
+        // Save updated stock details back to the database
         warehouseRepository.save(stock);
         log.info("warehouse has been updated with with " + stock);
     }
